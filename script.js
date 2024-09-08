7f33f35a27e732fb82f3877b99cb369bf3cd1f6d
EyramAnn <anndekonor@gmail.com>
2024-09-08 19:10:14 +0000

    created my portfolio website

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle the visibility of the navigation menu on mobile screens
+function toggleNav() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+  }
